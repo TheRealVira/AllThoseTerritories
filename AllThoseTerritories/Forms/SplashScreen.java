121dbedc0230217f6fc69ae84380ce052840a5f8
Thomas Rührig <tom.ruehrig@aon.at>
2016-01-01 17:30:28 +0000

    Init commit

---
+++ AllThoseTerritories/Forms/SplashScreen.java
@@ -0,0 +1,17 @@
+package Forms;
+
+import javax.swing.*;
+
+/**
+ * Created by Thomas on 01/01/2016.
+ */
+public class SplashScreen extends JFrame{
+
+    public SplashScreen(String resource) {
+        super("AllThoseTerritories");
+        setSize(1250,680); // We have to add 30 px to spacing issues with swing...
+        setResizable(false);
+        add(new JLabel(new ImageIcon(getClass().getClassLoader().getResource(resource))));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AllThoseTerritories
-Das Programm AllThoseTerritories ist ein Swing­basiertes Spiel für eine Person.  Ziel des Spiels ist es, alle Territorien unter die eigene Kontrolle zu bringen.
+Das Programm AllThoseTerritories ist ein Swing­basiertes Spiel für eine Person (wird aber erweitert werden, damit man es auch zu zweit Spielen kann!).  Ziel des Spiels ist es, alle Territorien unter die eigene Kontrolle zu bringen.
